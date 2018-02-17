2ceee016afb2ba07ad46d336221432ef11c15721
musti <malasadi@csumb.edu>
2018-02-17 17:18:06 +0000

    first commit

---
+++ Labs/lab2/file.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title> Lab2: 777 Slot Machine </title>
+    </head>
+    
+    
+    
+    <body>
+
+
+        <?php
+        
+        function displaySymbol($randomValue){
+            
+            //$randomValue=rand (0,2);
+            
+            echo $randomValue;
+        
+            if($randomValue==0) {
+                $symbol="seven";
+        
+          } else if ($randomValue==1){
+                 $symbol="orange";
+        
+           } else {
+                $symbol="cherry";
+        
+          }
+    
+              echo " <img src='img/$symbol.png' width='70' alt='$symbol' title='$symbol'> ";
+            
+        }
+        
+        
+        $randomValue1=rand(0,2);
+        displaySymbol($randomValue1);
+        
+          $randomValue2=rand(0,2);
+        displaySymbol($randomValue2);
+        
+          $randomValue3=rand(0,2);
+        displaySymbol($randomValue3);
+      
+         
+echo "<br/><hr> Values:$randomValue1 $randomValue2 $randomValue3 "
+         
+         
+
+      
+         
+         
+         
+         
+       
+         
+         
+         
+         
+         
+         
+         
+        
+        ?>
+
+
+    <!--    <img src="img/lemon.png" width="70" alt="Lemon" title="Lemon">
+        <img src="img/cherry.png" width="70" alt="cherry" title="cherry">
+        <img src="img/orange.png" width="70" alt="orange" title="orange">
+-->
+
+
+    </body>
+    
+    
+    
+</html>
+
+
+
+
+
+
+
+
+
+ /*
+        $randomValue = rand(0,2);
+        
+        if($randomValue==0){
+            echo '<img src="img/seven.png" alt="seven" title= "seven" width= "70"/>';
+        }
+        
+         if($randomValue==1){
+            echo '<img src="img/cherry.png" alt="cherry" title= "cherry" width= "70"/>';
+        }
+        
+         if($randomValue==2){
+            echo '<img src="img/lemon.png" alt="lemon" title= "lemon" width= "70"/>';
+        }
+        
+        */
---
+++ practice/Untitled1.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+    
+<head>
+    
+    <title>Title</title>
+    <meta charset="utf-8">
+
+
+  
+
+
+</head>
+
+
+
+
+
+    <body>
+
+
+
+        <?php
+            
+            $a=array("ten", "ace", "jack");
+            
+            
+            for ($i=0;$i<10;$i++){
+            echo " <img src = '../challenges/challenge2/img/cards/clubs/ten.png' /> " . "<br>" ;
+            
+            }
+
+        ?>
+
+
+    </body>
+
+
+
+
+
+
+
+</html>
