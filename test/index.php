5e50423e60adec2b99ef040d112df918f066a73a
musti <malasadi@csumb.edu>
2018-03-04 01:46:20 +0000

    a

--- test/index.php
+++ test/index.php
@@ -76,7 +76,7 @@
         ?>
         
         <?php
-        echo "You are visitor number $counterVal to this site";
+        echo "You are Fisitor number $counterVal to this site";
         ?>
         
         <br>  <br>  <br>
