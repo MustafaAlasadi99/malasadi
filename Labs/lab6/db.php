3bba8050eeb0fabe4aeefd1f04611cc22049a6a3
musti <malasadi@csumb.edu>
2018-04-01 00:21:57 +0000

    new

--- Labs/lab6/db.php
+++ Labs/lab6/db.php
@@ -10,8 +10,8 @@ $password = "elmaestro0";
 
 //checks whether the URL contains "herokuapp" (using Heroku)
 if(strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
-   $url = parse_url(getenv("us-cdbr-iron-east-05.cleardb.net/heroku_5b5b35991ee9557?reconnect=true"));
-   $host = $url["us-cdbr-iron-east-05.cleardb.net"];
+   $url = parse_url(getenv("mysql://b1462255794105:1a71dff1@us-cdbr-iron-east-05.cleardb.net/heroku_5b5b35991ee9557?reconnect=true"));
+   $host = $url["host"];
    $dbname = substr($url["path"], 1);
    $username = $url["b1462255794105"];
    $password = $url["1a71dff1"];
