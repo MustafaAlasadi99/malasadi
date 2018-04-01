8e3d1137c1d06155ae7420f9b6d25419907479ab
musti <malasadi@csumb.edu>
2018-04-01 02:12:57 +0000

    new

--- Labs/lab6/db.php
+++ Labs/lab6/db.php
@@ -1,6 +1,5 @@
 <?php
 
-
 function getDatabaseConnection($dbName) {
 
 $host = "localhost";
@@ -10,11 +9,11 @@ $password = "elmaestro0";
 
 //checks whether the URL contains "herokuapp" (using Heroku)
 if(strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
-   $url = parse_url(getenv("mysql://b1462255794105:1a71dff1@us-cdbr-iron-east-05.cleardb.net/heroku_5b5b35991ee9557?reconnect=true"));
+   $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
    $host = $url["host"];
    $dbname = substr($url["path"], 1);
-   $username = $url["b1462255794105"];
-   $password = $url["1a71dff1"];
+   $username = $url["user"];
+   $password = $url["pass"];
 }
 
 $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
@@ -23,7 +22,5 @@ $dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 return $dbConn;
 
 }
-
-
 
 ?>
